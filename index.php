fb2ae270423d8e282c2d1a2a77fed997c36c4f14
Javier Moreno <javiermorenov1203@gmail.com>
2025-10-24 16:23:15 -0300

    frontend v1.0

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <div class="main-container">
+        <div class="upper-container">
+            <img src="credifama_logo.svg" alt="credifama logo" class="credigama-logo">
+        </div>
+
+        <div class="bottom-container">
+            <div class="left-panel">
+                <h2 class="panel-title">Ingreso de datos</h2>
+                <form action="" class="data-form">
+                    <div>
+                        <label>Nombre*</label>
+                        <input type="text" class="data-input">
+                    </div>
+                    <div>
+                        <label>Email*</label>
+                        <input type="text" class="data-input">
+                    </div>
+                    <div>
+                        <label>Teléfono</label>
+                        <input type="text" class="data-input">
+                    </div>
+                    <input type="submit" value="Enviar" class="send-btn">
+                </form>
+            </div>
+            <span class="panel-separator"></span>
+            <div class="right-panel">
+                <h2 class="panel-title">Listado de datos</h2>
+                <div class="data-card">
+                    <span>Nombre: Javier Moreno</span>
+                    <span>Email: javier@gmail.com</span>
+                    <span>Teléfono: 096 715 450</span>
+                    <span>Ingresado: 10/24/2025</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
